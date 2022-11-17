c266b471570e90be89b161bd7e7928b3987e5633
RayanAlouzi <62511479+RayanAlouzi@users.noreply.github.com>
2022-11-17 09:33:45 -0700

    now cant input invalid json file

--- load-json.py
+++ load-json.py
@@ -3,6 +3,15 @@ from pymongo import MongoClient
 
 def load_json():
     json_file = input("Input a json file: ")
+    works = 0
+    while(works == 0):
+        try:
+            open(json_file)
+            works == 1
+        except: 
+            works == 0
+            json_file = input("Incorrect json file, try again: ")
+            
     port_num = input("Input a port number: ")
     client = MongoClient('mongodb://localhost:' + port_num)
 
@@ -15,6 +24,7 @@ def load_json():
     with open(json_file) as f: 
         for line in f: 
             data.append(json.loads(line.strip()))
+
     dblp.insert_many(data)
 if __name__ == "__main__":
     load_json()
