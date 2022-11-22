0924813ffd27f6f087d027c750fdaec8aca7c6d5
dreambigzhang <alan.yushu.zhang@gmail.com>
2022-11-22 15:02:12 -0700

    can display result if referenced for each article is hard-coded

--- listVenues.py
+++ listVenues.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 import pymongo
-import json
 def listVenues(db): 
     """
     List the venues The user should be able to enter a number n and see a listing of top n venues. For each venue, 
@@ -12,33 +11,31 @@ def listVenues(db):
     
     print("**List Top Venues")
     
-    topNum = input("Enter the number of top venues: ")
+    topNum = int(input("Enter the number of top venues: "))
     db.dblp.create_index([("reference",pymongo.TEXT)])
 
 
     cursor = db.dblp.aggregate([
-        {
-            "$lookup":
-                {
-                    "from":"dblp",
-                    "localField":"id",
-                    "foreignField":"references",
-                    "as":"referenced"
-                }
+       
+         {
+            "$addFields":
+            {
+                "referenced": 5 #db.dblp.count_documents({"$text": { "$search": "id"}})
+            }
         },
         {
             "$group" : {
                 "_id" : "$venue",
-                "artCount":{"$count":1},
-                "refCount": {"$size":"$referenced"},#{db.dblp.count_documents({"$text": { "$search": "id"}})}
+                "artCount":{"$count":{}},
+                "refCount": {"$sum":"$referenced"}#{db.dblp.count_documents({"$text": { "$search": "id"}})}
             }
         },
         {
             "$project":
-                {"venue":1,"artCount":1,"refCount": 1}
+                {"venue":1,"artCount":1, "refCount": 1} #,"refCount": 1
         },
         { 
-            "$sort": { "refCount": 1 } 
+            "$sort": { "refCount": -1 } 
         },
         {
             "$limit": topNum
@@ -47,3 +44,22 @@ def listVenues(db):
     for venue in cursor:
         print(venue)
     quit()
+'''
+ {
+            "$addFields":
+            {
+                "referenced": db.dblp.count_documents({"$text": { "$search": "id"}})
+            }
+        },
+'''
+'''
+        {
+            "$lookup":
+            {
+                "from":"dblp",
+                "localField":"id",
+                "foreignField":"references",
+                "as":"referenced"
+            }
+        },
+       '''
